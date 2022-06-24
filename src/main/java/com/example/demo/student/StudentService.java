c26b56a1bca0b4da5b9ea66e61ce8601611f2be5
ytolks <yevgeniy.tolks@gmail.com>
2022-06-24 14:15:18 +0300

    commit 4

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,21 +1,20 @@
 package com.example.demo.student;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Service
 public class StudentService {
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(new Student(
-                        1L,
-                        "Yevgeniy",
-                        "yevgeniy.tolks@gmail.com",
-                        LocalDate.of(1984, 7, 4),
-                        37
-                )
-        );
+        return studentRepository.findAll();
     }
 }
